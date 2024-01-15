57291283dab7bfe2cbb84332c483b5fc4d1b7917
lrmn7 <lrmn.dev@gmail.com>
2024-01-15 19:53:49 +0700

    ✨

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle 
 require('dotenv').config();
 const { Canvas, resolveImage } = require('canvas-constructor');
 const canvas = require('canvas');
+const { registerFont } = require('canvas');
+registerFont("./LuckiestGuy-Regular.ttf", { family: 'Luckiest Guy' });
 
 const client = new Client({
     intents: [
@@ -14,195 +16,143 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+
+    const activities = [
+        `Love has a cost, and both of us lost`,
+        `Mostly sleepless🌛`,
+        `Mewwme's Everywhere`,
+    ];
+    
+    setInterval(() => {
+        const randomActivity = activities[Math.floor(Math.random() * activities.length)];
+    
+        client.user.setPresence({
+            activities: [
+                {
+                    name: randomActivity,
+                    type: 4,
+                },
+            ],
+            status: "idle",
+        });
+    }, 5000);
 });
 
-client.on('guildMemberAdd', async (member) => {
-    const channelId = '1091287764252753981';
+const generateImage = async (member, channelId, isWelcome) => {
+    try {
+        const greetings = isWelcome ? ["Hi,", "Hallo,", "Heyho,", "Hola,"] : ["Adiós,", "Bye,", "Sayōnara,"];
+        const images = [
+    "https://cdn.is-a.fun/mewcard/themes7/1.png",
+    "https://cdn.is-a.fun/mewcard/themes7/2.png",
+    "https://cdn.is-a.fun/mewcard/themes7/3.png",
+    "https://cdn.is-a.fun/mewcard/themes7/4.png",
+    "https://cdn.is-a.fun/mewcard/themes7/5.png",
+    "https://cdn.is-a.fun/mewcard/themes7/6.png",
+    "https://cdn.is-a.fun/mewcard/themes7/7.png",
+    "https://cdn.is-a.fun/mewcard/themes7/8.png",
+    "https://cdn.is-a.fun/mewcard/themes7/9.png",
+    "https://cdn.is-a.fun/mewcard/themes7/10.png",
+    "https://cdn.is-a.fun/mewcard/themes7/11.png",
+    "https://cdn.is-a.fun/mewcard/themes7/12.png",
+    "https://cdn.is-a.fun/mewcard/themes7/13.png",
+    "https://cdn.is-a.fun/mewcard/themes7/14.png",
+    "https://cdn.is-a.fun/mewcard/themes7/15.png",
+    "https://cdn.is-a.fun/mewcard/themes7/16.png",
+    "https://cdn.is-a.fun/mewcard/themes7/17.png",
+    "https://cdn.is-a.fun/mewcard/themes7/18.png",
+    "https://cdn.is-a.fun/mewcard/themes7/19.png",
+    "https://cdn.is-a.fun/mewcard/themes7/20.png",
+    "https://cdn.is-a.fun/mewcard/themes7/21.png",
+    "https://cdn.is-a.fun/mewcard/themes7/22.png",
+    "https://cdn.is-a.fun/mewcard/themes7/23.png",
+    "https://cdn.is-a.fun/mewcard/themes7/24.png",
+    "https://cdn.is-a.fun/mewcard/themes7/25.png",
+    "https://cdn.is-a.fun/mewcard/themes7/26.png",
+    "https://cdn.is-a.fun/mewcard/themes7/27.png",
+    "https://cdn.is-a.fun/mewcard/themes7/28.png",
+    "https://cdn.is-a.fun/mewcard/themes7/29.png",
+    "https://cdn.is-a.fun/mewcard/themes7/30.png",
+    "https://cdn.is-a.fun/mewcard/themes7/31.png",
+    "https://cdn.is-a.fun/mewcard/themes7/32.png",
+    "https://cdn.is-a.fun/mewcard/themes7/33.png",
+    "https://cdn.is-a.fun/mewcard/themes7/34.png",
+    "https://cdn.is-a.fun/mewcard/themes7/35.png",]; 
+    const textColors = ['#f4e0c5', '#ff9200', '#9893fc', '#ff00ea', '#00ff18'];
 
-    if (!channelId) {
-        return;
-    }
-
-    const greetings = ["Hi,", "Hallo,", "Heyho,", "Hola,"];
-    const images = [
-        "https://cdn.is-a.fun/welgood/1.png",
-        "https://cdn.is-a.fun/welgood/2.png",
-        "https://cdn.is-a.fun/welgood/3.png",
-        "https://cdn.is-a.fun/welgood/4.png",
-        "https://cdn.is-a.fun/welgood/5.png",
-        "https://cdn.is-a.fun/welgood/6.png",
-        "https://cdn.is-a.fun/welgood/7.png",
-        "https://cdn.is-a.fun/welgood/8.png",
-        "https://cdn.is-a.fun/welgood/9.png",
-        "https://cdn.is-a.fun/welgood/10.png",
-        "https://cdn.is-a.fun/welgood/11.png",
-        "https://cdn.is-a.fun/welgood/12.png",
-        "https://cdn.is-a.fun/welgood/13.png",
-        "https://cdn.is-a.fun/welgood/14.png",
-        "https://cdn.is-a.fun/welgood/15.png",
-        "https://cdn.is-a.fun/welgood/16.png",
-        "https://cdn.is-a.fun/welgood/17.png",
-        "https://cdn.is-a.fun/welgood/18.png",
-        "https://cdn.is-a.fun/welgood/19.png",
-        "https://cdn.is-a.fun/welgood/20.png",
-        "https://cdn.is-a.fun/welgood/21.png",
-        "https://cdn.is-a.fun/welgood/22.png",
-        "https://cdn.is-a.fun/welgood/23.png",
-        "https://cdn.is-a.fun/welgood/24.png",
-        "https://cdn.is-a.fun/welgood/25.png",
-    ];
-
-    const textColors = [
-        "#FF0000",
-        "#00FF00", 
-        "#0000FF", 
-          "#FF0000", 
-          "#00FF00", 
-          "#0000FF", 
-          "#000080", 
-          "#00FFFF", 
-          "#00FF00", 
-          "#C0C0C0", 
-          "#FF00FF", 
-          "#000080", 
-          "#808000", 
-          "#008080", 
-          "#00FFFF", 
-          "#FFA500", 
-          "#63D1F4", 
-          "#7B3F00", 
-          "#8B008B", 
-          "#C1F0F6", 
-          "#CCCC00", 
-          "#CDE472", 
-          "#D1EEEE", 
-          "#FFB00F",
-          "#9fc8e5",
-    ];
-
+    const randomColor = textColors[Math.floor(Math.random() * textColors.length)];
     const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
     const randomImageIndex = Math.floor(Math.random() * images.length);
     const selectedImage = images[randomImageIndex];
-    const selectedColor = textColors[randomImageIndex];
 
     const img = await canvas.loadImage(selectedImage);
     const userPfp = await resolveImage(member.user.displayAvatarURL({
         extension: 'jpg',
         size: 1024,
     }));
-    const namee = `${randomGreeting} ${member.user.username}`;
+    const greetingText = `${randomGreeting}`;
+    const namee = member.user.username.length > 11
+        ? member.user.username.substring(0, 11) + '...'
+        : member.user.username;
 
-    const image = new Canvas(994, 198)
+    return new Canvas(994, 198)
         .printImage(img, 0, 0, 994, 198)
-        .setColor(selectedColor)
-        .setTextFont('54px sans-serif')
-        .printWrappedText(namee, 252, 102)
-        .printCircularImage(userPfp, 146, 97, 67, 67)
+        .setColor(randomColor)
+        .setTextFont('32px Luckiest Guy')
+        .printWrappedText(greetingText, 252, 85)
+        .setTextFont('54px Luckiest Guy')
+        .printWrappedText(namee, 252, 140)
+        .printCircularImage(userPfp, 146, 100, 67, 67)
         .toBuffer();
+} catch (error) {
+    console.error('Error generating image:', error);
+    throw error; // Meneruskan error ke penanganan lain jika diperlukan
+}
+};
+
+client.on('guildMemberAdd', async (member) => {
+try {
+    console.log(`Member joined: ${member.user.tag}`);
+    const channelId = process.env.WELCOME_CHANNEL_ID;
+
+    if (!channelId) {
+        return;
+    }
+
+    const image = await generateImage(member, channelId, true);
 
     const channel = await client.channels.fetch(channelId);
 
     const row1 = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
-                .setLabel("Mangex Hosted")
-                .setEmoji("<a:mooinvite:1167716601039159316>")
+                .setLabel("Mewwme's")
+                .setEmoji("<:singing:1196006929932234782>")
                 .setStyle(ButtonStyle.Link)
-                .setURL("https://discord.gg/SbqxZzUS3F")
+                .setURL("https://meww.me")
         );
 
     channel.send({
-        content: `<a:tragic5:1169269912989089872> <@${member.id}> <a:tragic5:1169269912989089872>`,
+        content: `Welcome to ${member.guild.name} <@${member.id}>, Stay in your imagination!`,
         files: [image],
         components: [row1],
     }).catch(error => {
-        console.error(error);
+        console.error('Error sending welcome message:', error);
     });
+} catch (error) {
+    console.error('Error handling guild member add event:', error);
+}
 });
 
 client.on('guildMemberRemove', async (member) => {
-    const channelId = '1091212076246110248';
+try {
+    console.log(`Member left: ${member.user.tag}`);
+    const channelId = process.env.LEAVE_CHANNEL_ID;
 
     if (!channelId) {
         return;
     }
 
-    const greetings = ["Adiós,", "Bye,", "Sayōnara,"];
-    const images = [
-        "https://cdn.is-a.fun/welgood/1.png",
-        "https://cdn.is-a.fun/welgood/2.png",
-        "https://cdn.is-a.fun/welgood/3.png",
-        "https://cdn.is-a.fun/welgood/4.png",
-        "https://cdn.is-a.fun/welgood/5.png",
-        "https://cdn.is-a.fun/welgood/6.png",
-        "https://cdn.is-a.fun/welgood/7.png",
-        "https://cdn.is-a.fun/welgood/8.png",
-        "https://cdn.is-a.fun/welgood/9.png",
-        "https://cdn.is-a.fun/welgood/10.png",
-        "https://cdn.is-a.fun/welgood/11.png",
-        "https://cdn.is-a.fun/welgood/12.png",
-        "https://cdn.is-a.fun/welgood/13.png",
-        "https://cdn.is-a.fun/welgood/14.png",
-        "https://cdn.is-a.fun/welgood/15.png",
-        "https://cdn.is-a.fun/welgood/16.png",
-        "https://cdn.is-a.fun/welgood/17.png",
-        "https://cdn.is-a.fun/welgood/18.png",
-        "https://cdn.is-a.fun/welgood/19.png",
-        "https://cdn.is-a.fun/welgood/20.png",
-        "https://cdn.is-a.fun/welgood/21.png",
-        "https://cdn.is-a.fun/welgood/22.png",
-        "https://cdn.is-a.fun/welgood/23.png",
-        "https://cdn.is-a.fun/welgood/24.png",
-        "https://cdn.is-a.fun/welgood/25.png",
-    ];
-
-    const textColors = [
-        "#FF0000",
-        "#00FF00", 
-        "#0000FF", 
-          "#FF0000", 
-          "#00FF00", 
-          "#0000FF", 
-          "#000080", 
-          "#00FFFF", 
-          "#00FF00", 
-          "#C0C0C0", 
-          "#FF00FF", 
-          "#000080", 
-          "#808000", 
-          "#008080", 
-          "#00FFFF", 
-          "#FFA500", 
-          "#63D1F4", 
-          "#7B3F00", 
-          "#8B008B", 
-          "#C1F0F6", 
-          "#CCCC00", 
-          "#CDE472", 
-          "#D1EEEE", 
-          "#FFB00F",
-          "#9fc8e5",
-    ];
-
-    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-    const randomImageIndex = Math.floor(Math.random() * images.length);
-    const selectedImage = images[randomImageIndex];
-    const selectedColor = textColors[randomImageIndex];
-
-    const img = await canvas.loadImage(selectedImage);
-    const userPfp = await resolveImage(member.user.displayAvatarURL({
-        extension: 'jpg',
-        size: 1024,
-    }));
-    const namee = `${randomGreeting} ${member.user.username}`;
-
-    const image = new Canvas(994, 198)
-        .printImage(img, 0, 0, 994, 198)
-        .setColor(selectedColor)
-        .setTextFont('54px sans-serif')
-        .printWrappedText(namee, 252, 102)
-        .printCircularImage(userPfp, 146, 97, 67, 67)
-        .toBuffer();
+    const image = await generateImage(member, channelId, false);
 
     const channel = await client.channels.fetch(channelId);
 
@@ -210,8 +160,15 @@ client.on('guildMemberRemove', async (member) => {
         content: `<@${member.user.id}>`,
         files: [image],
     }).catch(error => {
-        console.error(error);
+        console.error('Error sending goodbye message:', error);
     });
+} catch (error) {
+    console.error('Error handling guild member remove event:', error);
+}
+});
+
+client.on('error', (error) => {
+console.error('Client error:', error);
 });
 
 // Log in to Discord with your client's token
